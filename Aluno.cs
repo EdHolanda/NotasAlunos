23bb66841699d85a3abcb38e0cfb597167268bc4
Edilson Holanda <edilson_holanda@hotmail.com>
2021-06-21 15:17:17 -0300

    Adicionar arquivos de projeto.

---
+++ Aluno.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NotasAlunos
+{
+    public class Aluno
+    {
+        public string Nome {get; set;}
+        public List<double> Notas {get; set;}
+
+        public double CalculaMedia()
+        {
+            double soma = 0;
+            foreach (double nota in this.Notas)
+            {
+                soma += nota;
+            }
+            double media = soma / this.Notas.Count;
+            return (media);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NotasAlunos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var QtdNotas = 3;
+            List<Aluno> Alunos = new List<Aluno>();
+            Console.WriteLine("Digite a quantidade de alunos:");
+            int QtdAlunos = Convert.ToInt32(Console.ReadLine());
+            
+            for (int i = 0; i < QtdAlunos; i++)
+            {
+                var aluno = new Aluno();
+                Console.WriteLine($"Digite o nome do aluno {i + 1}");
+                aluno.Nome = Console.ReadLine();
+                aluno.Notas = new List<double>();
+                
+                for (int n = 0; n < QtdNotas; n++)
+                {
+                    Console.WriteLine($"Digite a nota {n + 1} do aluno {aluno.Nome}");
+                    aluno.Notas.Add(Convert.ToDouble(Console.ReadLine()));
+                }
+                Alunos.Add(aluno);
+
+            }
+            
+            Console.Clear();
+
+            foreach (var aluno in Alunos)
+            {
+                Console.WriteLine("=========================");
+                Console.WriteLine($"Nome {aluno.Nome}");
+                double notas = 0;
+                for (int i = 0; i < aluno.Notas.Count; i++)
+                {
+                    Console.WriteLine($"Nota {i + 1} - {aluno.Notas[i]: 0.0}");
+                    /*notas += aluno.Notas[i];*/
+                }
+
+                Console.WriteLine($"Média - {aluno.CalculaMedia(): 0.0}");
+            }
+            Console.WriteLine("=========================");
+        }
+
+    }
+}
